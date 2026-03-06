cee2084d989d45ceecc5a49deb9287158b879f69
Rebeca Gomes Ferreira <rebecagomes2208@gmail.com>
2026-03-06 11:05:03 -0300

    Add files via upload
    AULA 06/03

---
+++ Sexta/src/br/com/controle/Calculos.java
@@ -0,0 +1,20 @@
+
+package br.com.controle;
+
+public class Calculos {
+    private double valor;
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+    
+    public double calcularDobbro(){
+        return this.valor * 2;
+    }
+    
+    
+}
